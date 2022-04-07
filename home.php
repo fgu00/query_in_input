778fac64886947733a532e6a40bb6a8ad0d7b2d2
Russo Salvatore <russo.salvatore@osdbsesto>
2022-04-07 13:39:48 +0200

    add

--- home.php
+++ home.php
@@ -2,10 +2,12 @@
 include "/../conf/conf.php";
 include "/../lib/select.php";
 include "/../lib/stampa.php";
-echo "<form action='stampa.php' method='POST'>
+echo "<form action='home.php' method='POST'>
 query<input type='text' name='query'>
 <input type='submit' values='spedisci'>
 </form>";
-
+$conn=new mysqli($db["servername"],$db["usermame"],$db["pass"],$db["nameDB"]);
+$query=$_POST['query'];
+stampa(select($conn,$query));
 
 ?>
