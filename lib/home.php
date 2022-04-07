4d67d1bbee4c5496712e082245836298db8ac6df
Russo Salvatore <russo.salvatore@osdbsesto>
2022-04-07 12:54:23 +0200

    add

--- lib/home.php
+++ lib/home.php
@@ -3,5 +3,4 @@ echo "<form action='stampa.php' method='POST'>
 query<input type='text' name='query'>
 <input type='submit' values='spedisci'>
 </form>";
-
 ?>
--- lib/stampa.php
+++ lib/stampa.php
@@ -1,7 +1,7 @@
 <?php
-include "conf/conf.php";
 $query=$_POST['query'];
-$conn=getPOST("russo.salvatore.tave.osdb.it","c190_salvo","c190_primo","");
+include "/../conf/conf.php";
+$conn=getPOST("russo.salvatore.tave.osdb.it","c190_salvo","","c190_primo");
 if($conn->connect_error){
     die('connessione fallita : '.$conn->connect_error);   
 }else{
