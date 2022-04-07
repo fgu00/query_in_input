068095b1b4c6484a36e6868b0dd3ec81fc5dfb4d
Russo Salvatore <russo.salvatore@osdbsesto>
2022-04-07 12:37:23 +0200

    add

--- lib/stampa.php
+++ lib/stampa.php
@@ -1,5 +1,10 @@
 <?php
 include "conf.php";
-
-
+$query=$_POST['query'];
+$conn=getPOST("russo.salvatore.tave.osdb.it","c190_salvo",'c190_primo',"");
+if(!$conn->connect_error){
+    die('connessione fallita : '.$conn->connect_error);   
+}else{
+    echo 'conessione riuscita'; 
+}
 ?>
